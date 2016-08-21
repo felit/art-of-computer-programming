b32cd5949e8e37ea386dc4c2e827dd6633977a92
felit <felit123456>
2016-08-21 19:47:20 +0800

    测试其周期

--- chapter03/section3_1/algorithm_k.py
+++ chapter03/section3_1/algorithm_k.py
@@ -4,46 +4,51 @@ def k1(x):
     y = x / (10 ** 9)
     for i in range(0, y + 1):
         x = k2(x)
-        print 'y = %s' % i
-        # print x
+        # print 'num = %010d' % x
+        print '\'%010d' % x
+        # print 'y = %s' % i
+    return x
 
 
 def k2(x):
     z = x / (10 ** 8) % 10
     if z <= 0:
         x = k3(x)
-        print x
+        # print x
     if z <= 1:
         x = k4(x)
-        print x
+        # print x
     if z <= 2:
         x = k5(x)
-        print x
+        # print x
     if z <= 3:
         x = k6(x)
-        print x
+        # print x
     if z <= 4:
         x = k7(x)
-        print x
+        # print x
     if z <= 5:
         x = k8(x)
-        print x
+        # print x
     if z <= 6:
         x = k9(x)
-        print x
+        # print x
     if z <= 7:
         x = k10(x)
-        print x
+        # print x
     if z <= 8:
         x = k11(x)
-        print x
+        # print x
     if z <= 9:
         x = k12(x)
-        print x
+        # print x
     return x
 
 
 def k3(x):
+    """
+        确保>=5* 10**9
+    """
     num = 5 * 10 ** 9
     if x < num:
         return x + num
@@ -105,6 +110,10 @@ def k12(x):
 
 if __name__ == '__main__':
     # k1(6065038420)
-    k1(6065038420)
+    x = 6065038420
+    x = 1
+    for i in range(1,10000):
+        x = k1(x)
+
     # k1(1)
     # print len('%s' % 6065038420)
