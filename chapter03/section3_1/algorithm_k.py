595ecc0c76badf497a35a83d147afd909c91e6f7
felit <felit123456>
2016-08-21 19:02:42 +0800

    add k algorithm

---
+++ chapter03/section3_1/algorithm_k.py
@@ -0,0 +1,100 @@
+# -*- coding:utf8 -*-
+
+def k1(x):
+    y = x / (10 ** 9)
+    for i in range(0, y + 1):
+        x = k2(x)
+        print x
+
+
+def k2(x):
+    z = x / (10 ** 8) % 10
+    x = k3(x)
+    if z > 1:
+        x = k4(x)
+    if z > 2:
+        x = k5(x)
+    if z > 3:
+        x = k6(x)
+    if z > 4:
+        x = k7(x)
+    if z > 5:
+        x = k8(x)
+    if z > 6:
+        x = k9(x)
+    if z > 7:
+        x = k10(x)
+    if z > 8:
+        x = k11(x)
+    if z > 9:
+        x = k12(x)
+    return x
+
+
+def k3(x):
+    num = 5 * 10 ** 9
+    if x < num:
+        return x + num
+    else:
+        return x
+
+
+def k4(x):
+    x = x ** 2 / (10 ** 5) % (10 ** 10)
+    return x
+
+
+def k5(x):
+    l = 10 ** len('%s' % x)
+    x = (1001001001 * (10 ** l) + x) / (10 ** 10)
+    return x
+
+
+def k6(x):
+    if x < (10 ** 7):
+        x = x + 9814055677
+    else:
+        x = 10 ** 10 - x
+    return x
+
+
+def k7(x):
+    x = x % (10 ** 5) * (10 ** 5) + x / (10 ** 5)
+    return x
+
+
+def k8(x):
+    return k5(x)
+
+
+def k9(x):
+    result = 0
+    for i in range(1, len('%s' % x) + 1):
+        n = x % (10 ** i) / (10 ** (i - 1))
+        result += ((n - 1) * (10 ** i)) if n != 0 else 0
+    return result
+
+
+def k10(x):
+    return (x ** 2 + 9999) if x < 100000 else x - 99999
+
+
+def k11(x):
+    while (x < 10 ** 9):
+        l = len('%s' % x)
+        # TODO 有问题
+        x = 10 ** l + x
+    return x
+
+
+def k12(x):
+    x = x * (x - 1) % (10 ** 10)
+    return x
+
+# def k13(x):
+
+
+
+if __name__ == '__main__':
+    k1(6065038420)
+    # print len('%s' % 6065038420)
